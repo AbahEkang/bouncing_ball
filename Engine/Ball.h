5b9cc76693c0a6725ea2639eea924c77b96d1a99
charles abah <charles.abah@live.it>
2024-06-09 15:50:05 +0200

    get velocity for ball.

--- Engine/Ball.h
+++ Engine/Ball.h
@@ -14,7 +14,8 @@ public:
 	bool DoWallCollision(const RectF& walls);
 	void ReboundX();
 	void ReboundY();
-	RectF& GetRect() const;
+	RectF GetRect() const;
+	Vec2 GetVelocity() const;
 private:
 	static constexpr float radius{ 7.0f };
 	static constexpr Color color{ Colors::Red };
